d37c4861dbea2cdcbe1fce3a578e1d00f674c565
syifahasan <114904721+syifahasan@users.noreply.github.com>
2023-03-01 00:08:50 +0700

    Add files via upload

---
+++ section3/main.dart
@@ -0,0 +1,32 @@
+import 'latihanFlutterBasic/latihan_1.dart';
+import 'latihanFlutterBasic/latihan_2.dart';
+import 'latihanFlutterBasic/latihan_3.dart';
+import 'latihanFlutterBasic/latihan_4.dart';
+import 'latihanFlutterBasic/latihan_5.dart';
+import 'latihanFlutterBasic/latihan_6.dart';
+import 'latihanFlutterBasic/latihan_7.dart';
+import 'latihanFlutterBasic/latihan_8.dart';
+import 'latihanFlutterBasic/latihan_9.dart';
+import 'latihanFlutterBasic/latihan_10.dart';
+import 'latihanFlutterBasic/latihan_11.dart';
+import 'latihanFlutterBasic/latihan_12.dart';
+import 'latihanFlutterBasic/latihan_13.dart';
+import 'latihanFlutterBasic/latihan_14.dart';
+import 'latihanFlutterBasic/latihan_15.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: latihan_15(),
+    );
+  }
+}
